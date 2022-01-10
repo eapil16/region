e98a361fc333e0e3034afaa2a7baeb73663c2306
eapil16 <77381727+eapil16@users.noreply.github.com>
2022-01-10 21:50:16 +0300

    fixed

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,15 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
+    document.querySelector('body').addEventListener('click', (e) => {
+        if (e.target.closest('#switches-house')) {
+          document.querySelector('.to-house').classList.add('hidden-block'); 
+        }
+        if (e.target.closest('#switches-apartment')) {
+            document.querySelector('.to-house').classList.remove('hidden-block'); 
+          }
+        
+    });
   
 
 });
